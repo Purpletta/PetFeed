197b54579100fef865108d203186a1ae1c0e9d0f
Alexey Semenov <101121914+Purpletta@users.noreply.github.com>
2024-06-20 12:20:38 +0300

    Добавлены users

--- Код/petfeed_backend/backend/settings.py
+++ Код/petfeed_backend/backend/settings.py
@@ -37,13 +37,19 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework_simplejwt',
+    'drf_yasg',
+    'rest_framework.authtoken',
     'articles',
-    'drf_yasg'
+    'authorisation',
+
 ]
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
+
 }
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
